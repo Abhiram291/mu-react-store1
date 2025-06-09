ab25fd6fc173254d25417e74f912153d2e1cac89
“Abhiram291” <abhiramayla@gmail.com>
2025-06-09 21:09:17 +0530

    update App.jsx to include new feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 import Login from "./Login";
 import Register from "./Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home" ;
+import Home from "./Home";
 function App() {
   return (
     <div>
